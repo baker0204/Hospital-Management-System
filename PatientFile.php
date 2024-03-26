f5d0a07daa52dc180c49190ac82aa36ffe032089
DiabloThe14 <callumkclark@gmail.com>
2024-03-26 13:27:10 -0500

    Update PatientFile.php
    Finished Patient Information Section

--- PatientFile.php
+++ PatientFile.php
@@ -13,13 +13,17 @@
 		<h4 align="center"> Siloam Springs, Arkansas, 72761</h3>
 		<h2> Patient Information</h2>
 			Name: <input name="patientName" id="patientName"/> 
-			Age: <input type="number" id="patientAge" name="patientAge" min="0" max="1" step="0"/>
-			Address: <input name="patientAddress" id="patientAddress"/> 
+			Date of Birth: <input type="date" name="patientDOB" id="patientDOB"/><br><br>
 			Sex: <select name="patientSex" id="patientSex" required> 
-			<option value = ""> </option>
-			<option value = "male"> Male </option>
-			<option value = "female"> Female </option>
-			<option value = "perferNot"> Perfer Not To Say </option>
+				<option value = ""> </option>
+				<option value = "male"> Male </option>
+				<option value = "female"> Female </option>
+				<option value = "perferNot"> Perfer Not To Say </option>
+			</select>
+			Phone Number: <input type="tel" id="patientPhone" name="patientPhone"/><br><br>
+			Emergency Contact: <textarea name="patientAddress" id="patientAddress"></textarea> 
+			Address: <textarea name="patientAddress" id="patientAddress"></textarea> 
+			<hr>
 		<h2> Family History</h2>
 		<h2> Allergies</h2>
 		<h2> Medications and Supplements</h2>
