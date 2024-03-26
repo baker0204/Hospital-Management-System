64fc812072002f65215365ac703ade9483437f39
baker0204 <71851398+baker0204@users.noreply.github.com>
2024-03-26 15:04:08 +0000

    Main Page

---
+++ main.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Hospital Manager</title>
+    <link rel="stylesheet" href="css/css.css"/>
+    <!--<script type="text/javascript" src="js/js.js"></script>-->
+</head>
+<body bgcolor="#eceff4">
+<center>
+    <img src="img/staroflife.png" width="10%">
+    <br>
+    <h1 align="center"> St. Athanasius Hospital </h1>
+    <ul>
+        <li class="menu-item"><a href="#" class="drp"> Appointment Management </a>
+            <div class="menu-content">
+                <a href="newappoint.php">New Appointment</a><br>
+                <a href="updateappoint.php">Update Appointment</a><br>
+                <a href="viewappoint.php">View All Appointments</a><br>
+            </div>
+        </li>
+
+        <li class="menu-item"><a href="#" class="drp"> Patient Management </a>
+            <div class="menu-content">
+                <a href="newpatient.php">New Patient</a><br>
+                <a href="updatepatient.php">Update Patient</a><br>
+                <a href="billpatient.php">Bill Patient</a><br>
+                <a href="viewpatients.php">View All Patients</a><br>
+            </div>
+        </li>
+
+        <li class="menu-item"><a href="#" class="drp">Doctor Management </a>
+            <div class="menu-content">
+                <a href="adduser.php">Add Doctor</a><br>
+                <a href="removeuser.php">Remove Doctor</a><br>
+                <a href="displayusers.php">Display all Doctors</a><br>
+            </div>
+        </li>
+    </ul>
+    <p align="center" style="font-family: monospace;"> Hospital Management brought to you by Baker and Callum. <br> Logo From Wikimedia Commons<br> 2024 No Rights Reserved...<p>
+</center>
+</body>
+</html>
