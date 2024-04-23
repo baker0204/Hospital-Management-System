211528dc4a1204daf74b36454692ecc037fc6b2e
DiabloThe14 <callumkclark@gmail.com>
2024-04-22 23:12:20 -0500

    Fixed Textbox Removal

--- js/js.js
+++ js/js.js
@@ -3,11 +3,20 @@ function add(div)
 	var input = document.createElement('input'); 
 	var input2 = document.createElement('input'); 
 	input.type = "text"; 
+	input2.type = "text"; 
 	var container = document.getElementById(div);
 	container.appendChild(input); 
 	container.appendChild(input2); 
 	var br = document.createElement('br');
 	container.appendChild(br);
+}
+
+function remove(div)
+{
+	var container = document.getElementById(div);
+	container.removeChild(container.lastChild);
+	container.removeChild(container.lastChild);
+	container.removeChild(container.lastChild);
 }
 
 function clearForm(formID)
