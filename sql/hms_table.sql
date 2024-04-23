0e45733f64ec5e3c480b64bb85f3b7a649955a4a
baker0204 <71851398+baker0204@users.noreply.github.com>
2024-04-23 15:03:40 +0000

    Update hms_table.sql

--- sql/hms_table.sql
+++ sql/hms_table.sql
@@ -71,7 +71,7 @@ INSERT INTO `doctor_table` (`did`, `name`, `sex`, `specialty`, `address`, `email
 (2, 'Dr. Baker Loften', 'male', 'Pediatrics', '10 Modest Way.', 'dr.loften@hospital.com', '5018592313'),
 (3, 'Dr. Callum Clark', 'male', 'Radiology', '78 Doctor Rd.', 'dr.clark@hospital.com', '4792346874'),
 (4, 'Dr. Megan Lavender', 'female', 'Anesthesiology', '50 Golfer Ln.', 'dr.lavender@hospital.com', '5018862145');
-(4, 'Dr. Mason Cary', 'male', 'Podiatry', '50 Tennis Ln.', 'dr.cary@hospital.com', '5018862145');
+(4, 'Dr. Mason Cary', 'male', 'Podiatry', '50 Tennis Ln.', 'dr.cary@hospital.com', '7788962541');
 
 -- --------------------------------------------------------
 
