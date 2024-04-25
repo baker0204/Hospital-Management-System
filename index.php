5f69054a4081bbe1d05053e7b1da091482e668cd
baker0204 <71851398+baker0204@users.noreply.github.com>
2024-04-25 14:51:18 +0000

    "Better" Looking Login Page

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <html>
 	<head>
-		<title>Login</title>
+		<title>St. Athanasius Login</title>
 		
 		<link rel="stylesheet" href="css/css.css"/>
 			
@@ -8,12 +8,21 @@
 	</head>
 
 	<body>
+	<center>
+	<br>
+	<img src="img/staroflife.png" width="10%">
+    <br>
+    <h1 align="center"> St. Athanasius Hospital </h1>
+	<br><br>
 	<form id="login" method="POST" action="redirect.php">
 		<table border=0 width=25%>
-			<tr><td>Username:</td><td><input type="text" name="uid" id="uid"/></td></tr>
-			<tr><td>Password:</td><td><input type="password" name="pwd" id="pwd" /></td></tr>
+			<tr><td><p style="font-family: monospace;"> Username:</td><td><input type="text" name="uid" id="uid" style="font-family: monospace;"/></td></tr>
+			<tr><td><p style="font-family: monospace;">Password:</td><td><input type="password" name="pwd" id="pwd" style="font-family: monospace;"/></td></tr>
 			<tr><td></td><td><input type="submit" value="Login" />    <input type="button" value="Reset" onclick="clearForm('login')"/></td></tr>
 		</table>
 	</form>
+	</center>
+	<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+	<p align="center" style="font-family: monospace;"> Hospital Management brought to you by Baker and Callum. Logo From Wikimedia Commons. 2024 No Rights Reserved...<p>
 	</body>
 </html>
